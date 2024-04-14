0d6cafece40cf82c2b5e1e80e11f8cfe5da8a97d
alexrefshauge <arefsh22@student.aau.dk>
2024-04-14 14:39:36 +0200

    add apptray and app view

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,11 @@ import "./App.css";
 import { Outlet, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
-import { Stack } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import AppTray from "./components/AppTray";
 import HomeView from "./components/appViews/HomeView";
+import AppView from "./components/appViews/AppView";
+import PenaltiesView from "./components/appViews/PenaltiesView";
 
 function App() {
 
@@ -15,7 +17,9 @@ function App() {
         <Route path="login" element={<LoginPage />} />
         <Route path="register" element={<RegisterPage />} />
         <Route path="app" element={<InnerApp />}>
-          <Route path="home" element={<HomeView />}></Route>
+          <Route path="home" element={<HomeView />} />
+          <Route path="penalties" element={<PenaltiesView />} />
+          <Route path="*" element={<NoView />} />
         </Route>
       </Routes>
     </Router>
@@ -32,4 +36,12 @@ function InnerApp() {
       <Outlet />
     </Stack>
   )
+}
+
+function NoView() {
+  return <AppView title="404">
+    <Typography>
+      Page does not exist
+    </Typography>
+  </AppView>
 }
--- frontend/src/components/AppTray.tsx
+++ frontend/src/components/AppTray.tsx
@@ -1,23 +1,94 @@
-import { AnnouncementSharp } from "@mui/icons-material"
-import { Button, Paper, Stack } from "@mui/material"
+import { HouseSharp, RequestQuoteSharp, WorkspacesSharp } from "@mui/icons-material";
+import {
+  Avatar,
+  Box,
+  Button,
+  Card,
+  Fade,
+  IconButton,
+  Link,
+  Paper,
+  Popper,
+  Stack,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import { useState } from "react";
+import { Link as RouterLink } from "react-router-dom";
 
-function AppTrayButton(props: {icon: React.ReactElement}) {
+function AppTrayButton(props: { to: string, icon: React.ReactElement }) {
     return (
-        <Button variant="outlined" color="secondary" sx={{height: "4rem"}}>
-            {props.icon}
+    <Link component={RouterLink} to={props.to}>
+        <Button variant="outlined" color="secondary" sx={{ height: "4rem" }}>
+        {props.icon}
         </Button>
-    )
+    </Link>
+  );
 }
 
 function AppTray() {
+  const [accountPopperAnchor, setAccountPopperAnchor] =
+    useState<null | HTMLElement>(null);
 
-    return (
-        <Paper sx={{backgroundColor: "background.default"}}>
-            <Stack height={"100%"} minWidth={"1rem"} padding={1} gap={2}>
-                <AppTrayButton icon={<AnnouncementSharp />} />
-            </Stack>
-        </Paper>
-    )
+  const handleToggleAccountPopper = (e: React.MouseEvent<HTMLElement>) => {
+    setAccountPopperAnchor(accountPopperAnchor ? null : e.currentTarget);
+  };
+
+  const accountPopperOpen = Boolean(accountPopperAnchor);
+
+  return (
+    <Paper sx={{ backgroundColor: "background.default" }}>
+      <Stack
+        height={"100%"}
+        boxSizing={"content-box"}
+        direction={"column"}
+        justifyContent={"space-between"}
+      >
+        <Stack height={"100%"} minWidth={"1rem"} padding={1} gap={1}>
+          <AppTrayButton to="/app/home" icon={<HouseSharp />} />
+          <AppTrayButton to="/app/penalties" icon={<RequestQuoteSharp />} />
+          <AppTrayButton to="/app/teams" icon={<WorkspacesSharp />} />
+        </Stack>
+        <Box
+          display={"flex"}
+          padding={1}
+          height={"4rem"}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <IconButton
+            type="button"
+            color="info"
+            onClick={handleToggleAccountPopper}
+          >
+            <Avatar sx={{ backgroundColor: "secondary.main" }}>AR</Avatar>
+          </IconButton>
+        </Box>
+      </Stack>
+
+
+
+      <Popper
+        open={accountPopperOpen}
+        anchorEl={accountPopperAnchor}
+        placement="right-end"
+        transition
+      >
+        {({ TransitionProps }) => (
+          <Fade {...TransitionProps} timeout={100}>
+            <Card sx={{marginLeft: "2rem"}}>
+                <Stack direction={"column"} padding={2} gap={2}>
+                    <Typography>Account</Typography>
+                    <Link component={RouterLink} to={"/login"}>
+                        <Button variant="contained" color="error">Logout</Button>
+                    </Link>
+                </Stack>
+            </Card>
+          </Fade>
+        )}
+      </Popper>
+    </Paper>
+  );
 }
 
-export default AppTray
+export default AppTray;
