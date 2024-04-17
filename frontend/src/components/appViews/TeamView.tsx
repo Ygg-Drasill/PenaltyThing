dd51a300197515c2da6ca37cd98231342912742d
Tobias Bay <tbay22@student.aau.dk>
2024-04-17 17:36:56 +0200

    Added icons to team

--- frontend/src/components/appViews/TeamView.tsx
+++ frontend/src/components/appViews/TeamView.tsx
@@ -1,6 +1,9 @@
 import { Link as RouterLink, Outlet } from "react-router-dom"
 import AppView from "./AppView"
-import { Button, Link, Stack } from "@mui/material"
+import {Button, Link, Stack, Typography, Box} from "@mui/material"
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import GroupAddIcon from '@mui/icons-material/GroupAdd';
 
 function TeamView() {
     return (
@@ -13,10 +16,34 @@ function TeamView() {
 function TeamViewNavbar() {
 
     return (
-        <Stack width={"100%"} direction={"row"} justifyContent={"flex-end"} gap={4}>
-            <Link component={RouterLink} to={"/app/team/list"} draggable={false}><Button variant="outlined">List</Button></Link>
-            <Link component={RouterLink} to={"/app/team/create"} draggable={false}><Button variant="outlined">Create</Button></Link>
-            <Link component={RouterLink} to={"/app/team/join"} draggable={false}><Button variant="outlined">Join</Button></Link>
+        <Stack width={"100%"} direction="row" justifyContent={"flex-end"} gap={4}>
+            <Link component={RouterLink} to={"/app/team/list"} draggable={false}>
+                <Button>
+                    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                        <FormatListBulletedIcon style={{ fontSize: 40 }} />
+                        <Typography variant="outlined">List</Typography>
+                    </Box>
+                </Button>
+            </Link>
+
+            <Link component={RouterLink} to={"/app/team/list"} draggable={false}>
+                <Button>
+                    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                        <AddCircleIcon style={{ fontSize: 40 }} />
+                        <Typography variant="outlined">Create</Typography>
+                    </Box>
+                </Button>
+            </Link>
+
+            <Link component={RouterLink} to={"/app/team/list"} draggable={false}>
+                <Button>
+                    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                        <GroupAddIcon style={{ fontSize: 40 }} />
+                        <Typography variant="outlined">Join</Typography>
+                    </Box>
+                </Button>
+            </Link>
+
         </Stack>
     )
 }
