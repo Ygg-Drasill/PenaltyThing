3d4f3421b3a14aea8034aab0734927c2bf592a7e
Tobias Bay <tbay22@student.aau.dk>
2024-04-19 17:51:27 +0200

    Redirecting to login page if no user is found in cookies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,36 @@
 import "./App.css";
 
-import { Navigate, Outlet, Route, BrowserRouter as Router, Routes, useNavigate } from "react-router-dom";
-import RegisterPage from "./pages/RegisterPage";
-import LoginPage from "./pages/LoginPage";
 import { Stack, Typography } from "@mui/material";
-import AppTray from "./components/AppTray";
-import HomeView from "./components/appViews/HomeView";
-import AppView from "./components/appViews/AppView";
-import PenaltiesView from "./components/appViews/PenaltiesView";
+import { useQuery } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useEffect, useState } from "react";
+import {
+  Navigate,
+  Outlet,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
 import Cookies from "universal-cookie";
-import TeamView from "./components/appViews/TeamView";
-import { useTeamServiceGetTeamsByUserIdKey, useUserServiceGetUserKey } from "./components/openapi/queries";
+import AppTray from "./components/AppTray";
+import AppView from "./components/appViews/AppView";
+import HomeView from "./components/appViews/HomeView";
+import PenaltiesView from "./components/appViews/PenaltiesView";
 import TeamCreatePage from "./components/appViews/teamPages/TeamCreatePage";
 import TeamJoinPage from "./components/appViews/teamPages/TeamJoinPage";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { AppContext } from "./components/hooks/appContext";
-import { TeamService, UserService } from "./components/openapi/requests";
-import { useQuery } from "@tanstack/react-query";
 import TeamListPage from "./components/appViews/teamPages/TeamListPage";
+import TeamView from "./components/appViews/TeamView";
+import { AppContext } from "./components/hooks/appContext";
+import {
+  useTeamServiceGetTeamsByUserIdKey,
+  useUserServiceGetUserKey,
+} from "./components/openapi/queries";
+import { TeamService, UserService } from "./components/openapi/requests";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
-
   return (
     <>
       <Router>
@@ -38,7 +47,7 @@ function App() {
             </Route>
             <Route path="*" element={<NoView />} />
           </Route>
-          <Route path="*" element={<Navigate replace to={"/app/home"}/>} />
+          <Route path="*" element={<Navigate replace to={"/app/home"} />} />
         </Routes>
       </Router>
       <ReactQueryDevtools initialIsOpen={false} />
@@ -48,51 +57,58 @@ function App() {
 
 export default App;
 
-function InnerApp() { //TODO: Introduce userSession context where user cookie is passed down to components inside innerApp
-  const navigate = useNavigate()
-  const [userId, setUserId] = useState("")
-  
-  const {data: user} = useQuery({
+function InnerApp() {
+  //TODO: Introduce userSession context where user cookie is passed down to components inside innerApp
+  const navigate = useNavigate();
+  const [userId, setUserId] = useState("");
+
+  const { data: user } = useQuery({
     queryKey: [useUserServiceGetUserKey],
     queryFn: () => {
-      return UserService.getUser({id: userId})
+      return UserService.getUser({ id: userId });
     },
-    enabled: !!userId
-  })
+    enabled: !!userId,
+  });
 
-  const {data: teams} = useQuery({
+  const { data: teams } = useQuery({
     queryKey: [useTeamServiceGetTeamsByUserIdKey],
     queryFn: () => {
-      return TeamService.getTeamsByUserId({userId: userId})
+      return TeamService.getTeamsByUserId({ userId: userId });
     },
-    enabled: !!userId
-  })
-  
+    enabled: !!userId,
+  });
+
   useEffect(() => {
-    const cookies = new Cookies()
-    const userId = cookies.get("userId")
-    if (!userId) {
-      navigate("/login")
+    const cookies = new Cookies();
+    const userId = cookies.get("userId");
+
+    if (cookies.get("userId") == 0 || !userId) {
+      navigate("/login");
     } else {
-      setUserId(userId)
+      setUserId(userId);
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, [navigate]);
 
   return (
-    <AppContext.Provider value={{user: user, teams: teams}}>
-      <Stack direction={"row"} height={"100vh"} padding={4} gap={4} boxSizing={"border-box"}>
-        <AppTray user={user} isLoading={!!user}/>
-        <Outlet context={{user}} />
+    <AppContext.Provider value={{ user: user, teams: teams }}>
+      <Stack
+        direction={"row"}
+        height={"100vh"}
+        padding={4}
+        gap={4}
+        boxSizing={"border-box"}
+      >
+        <AppTray user={user} isLoading={!!user} />
+        <Outlet context={{ user }} />
       </Stack>
     </AppContext.Provider>
-  )
+  );
 }
 
 function NoView() {
-  return <AppView title="404">
-    <Typography>
-      Page does not exist
-    </Typography>
-  </AppView>
+  return (
+    <AppView title="404">
+      <Typography>Page does not exist</Typography>
+    </AppView>
+  );
 }
