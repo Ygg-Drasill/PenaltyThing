a0c518691c9af01eb7d6eb33df7c1637467269be
Tobias Bay - Windows <tobiasabay@gmail.com>
2024-09-25 11:40:25 +0200

    Change routeing to load list instead of pressing button + Included pretittier file for configuration

--- frontend/src/components/appViews/TeamView.tsx
+++ frontend/src/components/appViews/TeamView.tsx
@@ -1,38 +1,45 @@
-import { Link as RouterLink, Outlet } from "react-router-dom"
-import AppView from "./AppView"
-import {Button, Link, Stack, Typography} from "@mui/material"
 import AddCircleIcon from '@mui/icons-material/AddCircle';
-import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import GroupAddIcon from '@mui/icons-material/GroupAdd';
-import {ReactNode} from "react";
+import { Button, Link, Stack, Typography } from '@mui/material';
+import { ReactNode } from 'react';
+import { Navigate, Outlet, Link as RouterLink } from 'react-router-dom';
+import AppView from './AppView';
 
 function TeamView() {
-    return (
-        <AppView title={"Team"} barComponent={<TeamViewNavbar />}>
-            <Outlet/>
-        </AppView>
-    )
+  return (
+    <AppView title={'Team'} barComponent={<TeamViewNavbar />}>
+      <Navigate to='list'></Navigate>
+      <Outlet />
+    </AppView>
+  );
 }
 
 function TeamViewNavbar() {
-    return (
-        <Stack width={"100%"} direction="row" justifyContent={"flex-end"} gap={4}>
-            <IconWithText path="/app/teams/list" icon={<FormatListBulletedIcon />} name="List" />
-            <IconWithText path="/app/teams/create" icon={<AddCircleIcon />} name="Create" />
-            <IconWithText path="/app/teams/join" icon={<GroupAddIcon />} name="Join" />
-        </Stack>
-    );
+  return (
+    <Stack width={'100%'} direction='row' justifyContent={'flex-end'} gap={4}>
+      <IconWithText
+        path='/app/teams/create'
+        icon={<AddCircleIcon />}
+        name='Create'
+      />
+      <IconWithText
+        path='/app/teams/join'
+        icon={<GroupAddIcon />}
+        name='Join'
+      />
+    </Stack>
+  );
 }
 
-function IconWithText(props: {path: string, icon: ReactNode, name: string}){
-    return(
-        <Link component={RouterLink} to={props.path} draggable={false}>
-            <Button>
-                {props.icon}
-                <Typography sx={{marginLeft: 0.5}}>{props.name}</Typography>
-            </Button>
-        </Link>
-    )
+function IconWithText(props: { path: string; icon: ReactNode; name: string }) {
+  return (
+    <Link component={RouterLink} to={props.path} draggable={false}>
+      <Button>
+        {props.icon}
+        <Typography sx={{ marginLeft: 0.5 }}>{props.name}</Typography>
+      </Button>
+    </Link>
+  );
 }
 
-export default TeamView
+export default TeamView;
